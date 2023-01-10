e27b6e8905d741dcdab69a4f26712fe7907c4981
dexiln <dexiln@yahoo.com>
2023-01-10 23:58:18 +0300

    Add simple tests

--- board_django/posts/tests.py
+++ board_django/posts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import unittest
 
+from django.test import TestCase
+from .models import Post
+from .views import HomePageView
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='This is a test')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'This is a test')
+
+class testViews(unittest.TestCase()):
+    def test_homepage(self):
+        result = HomePageView.template_name
+        self.assertEqual(result,'home.html')
